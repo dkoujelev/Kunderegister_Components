b59d892c87fc5e7b0b45bef4219d8d164a89ef32
Dmitryko97 <dkoujelev97@gmail.com>
2018-03-13 20:24:11 +0100

    Setting up project

---
+++ src/Components/GroupListItem.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import '../Style/GroupListItemStyle.css';
+
+export interface Props {
+  name?: string;
+  leadsTotal: number;
+  leadsContacted: number;
+  streak?: number;
+  rising?: false | true;
+  sinking?: false | true;
+  contactedByYou?: number;
+  top?: false | true;
+}
+
+class GroupListItem extends React.Component<Props> {
+  public static defaultProps: Partial<Props> = {
+    name: 'Name not found',
+    leadsTotal: 0,
+    leadsContacted: 0,
+    streak: 0,
+    contactedByYou: 0
+  };
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  getProgress() {
+    return Math.round(100 * this.props.leadsContacted / this.props.leadsTotal);
+  }
+
+  getStatus() {
+    const { streak, top, rising, sinking, contactedByYou } = this.props;
+    let status = '';
+
+    if (streak && streak >= 3) { status += streak + '🔥'; }
+    if (top) { status += '👑'; }
+    if (rising) { status += '🚀'; }
+    if (sinking) { status += '🥀'; }
+
+    if (contactedByYou) {
+      if (contactedByYou > 100) {
+        status += '🤩';
+      } else if (contactedByYou >= 50) {
+        status += '😎';
+      } else if (contactedByYou >= 20) {
+        status += '😃';
+      } else if (contactedByYou >= 1) {
+        status += '🙂';
+      }
+    }
+
+    return status;
+  }
+
+  render() {
+    return (
+      <li className="listItem">
+        <div className="groupInfo">
+          <p className="groupName">{this.props.name}</p>
+          <p className="progress">{this.getProgress()}% fullført</p>
+        </div>
+        <div className="statusField">
+          <p>{this.getStatus()}</p>
+        </div>
+
+      </li>
+    );
+  }
+}
+
+export default GroupListItem;
